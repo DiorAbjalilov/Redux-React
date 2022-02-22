933d27042256a54ea603b6ee3887b7e9f7ab3fb7
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-02-22 23:57:15 +0500

    fixx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-const initialState = {
-  counter:0,
-};
 export default function App() {
   const [value, setValue]=useState('')
   const dispach = useDispatch();
   const counter = useSelector(state=>state);
   console.log(counter);
-  const HandlerClick=()=>{
-    return dispach({ type: "Increment", payload: value})
+  const HandlerClick=(e)=>{
+    if(e.key==='Enter'){
+      dispach({ type: "Increment", payload: value})
+      setValue('')
+    }
   }
   return (
     <div>
-      <input type='text' onChange={(e)=>setValue(e.target.value)} value={value} />
-      <button onClick={HandlerClick}>Enter</button>
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={(e)=>HandlerClick(e)}
+        value={value}
+      />
+      {/* <button onClick={HandlerClick}>Enter</button> */}
       {/* <button onClick={() => dispach({ type: "Clear" })}>Clear</button> */}
       {/* <button onClick={() => dispach({ type: "Decrement" })}>Decrement</button> */}
     </div>
