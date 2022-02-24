411294bda3c5dc610bddc86159d2c04922bc4008
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-02-24 22:21:30 +0500

    react redux

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import {Provider} from 'react-redux'
 import HookCakeContainer from './components/HookCakeContainer'
 import IceCreamContent from './components/IceCreamContent'
 import NewCakeContent from './components/NewCakeContent'
+import ItemContent from './components/ItemContent'
+import UserContent from './components/UserContent'
 
 const App = () => {
   return (
     <Provider store={store}>
+      <UserContent/>
+      {/* <ItemContent cake/>
         <CakeContent/>
         <HookCakeContainer/>
         <IceCreamContent/>
-        <NewCakeContent/>
+        <NewCakeContent/> */}
     </Provider>
   )
 }
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,11 @@
 import {combineReducers} from 'redux'
 import cakeReducer from './cake/cakeReducer'
 import iceCreamReducer from './iceCream/iceCreamReduser'
-
+import userReducer from './user/userReducer'
 const rootReducer=combineReducers({
     cake:cakeReducer,
-    iceCream:iceCreamReducer
+    iceCream:iceCreamReducer,
+    user:userReducer
 })
 
 export default rootReducer
