26c478e2f8772aee08860ffd3c68dbe65beffacc
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-03-09 22:56:47 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react'
-import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './actions'
+import React from "react";
+import "./App.css";
+import Components from "./components";
 const App = () => {
-  const counter=useSelector(state=>state.counter)
-  const login=useSelector(state=>state.isLogged)
-  const dispatch=useDispatch()
   return (
     <>
-      <h2>Counter: {counter}</h2>
-      <button onClick={()=>dispatch(increment(5))} >+</button>
-      <button onClick={()=>dispatch(decrement())}>-</button>
-      {login ? <p>log in</p>:''}
+      <Components />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
-import { createStore} from 'redux'
-import { Provider } from 'react-redux'
-import allReducers from "./reducers";
-const store=createStore(
-    allReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const app = (
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
+import "rsuite/dist/rsuite.min.css";
+const app = <App />;
 render(app, document.getElementById("root"));
